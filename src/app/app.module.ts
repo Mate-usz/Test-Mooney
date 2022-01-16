e1fd7515018973a4c2e50d150346451bedf4ac1b
Mateusz Rodz <50263944+Mate-usz@users.noreply.github.com>
2022-01-17 00:12:36 +0100

    feat: dashboard + some media query

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './modules/dashboard/components/product/product.component';
-import { ListProductComponent } from './modules/dashboard/components/list-product/list-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductComponent,
-    ListProductComponent
+    AppComponent
   ],
   imports: [
     CommonModule,
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardContainerComponent } from './pages/dashboard-container/dashboard-container.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { ProductComponent } from './components/product/product.component';
+import { ListProductComponent } from './components/list-product/list-product.component';
 
 @NgModule({
   declarations: [
-    DashboardContainerComponent
+    DashboardContainerComponent,
+    ProductComponent,
+    ListProductComponent
   ],
   imports: [
     CommonModule,
